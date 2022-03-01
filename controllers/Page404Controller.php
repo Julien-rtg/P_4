002c82a926ba43f7ee3ed5c775b67167d9717800
Julien-Rtg <julien.rittl@gmail.com>
2022-03-01 12:02:06 +0100

    page 404

---
+++ controllers/Page404Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controllers;
+
+use Controllers\core\MainController;
+
+
+class Page404Controller extends MainController
+{
+
+    // $this->main_view = ./views/main.html/twig and is define in MainController
+    public function renderPage404()
+    {
+        echo $this->twig->render($this->main_view, [
+            'body' => 'twig/Page404.html.twig'
+        ]);
+    }
+
+}
