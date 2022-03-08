5d7697ad9b4d29570915a6b86e801e349b96c6dd
Julien-Rtg <julien.rittl@gmail.com>
2022-03-08 10:48:33 +0100

    connection to database

--- db/Database.php
+++ db/Database.php
@@ -2,6 +2,31 @@
 
 namespace App;
 
-class Database {
+use App\Config;
+
+class Database extends Config {
     
+    public $pdo;
+    private $credits;
+
+    public function __construct()
+    {
+        $this->credits = $this->getCredits();
+    }
+
+    private function connect(){
+        try {
+            $dbh = new \PDO('mysql:host='.$this->credits['host'].';dbname='.$this->credits['db_name'], $this->credits['user'], $this->credits['pass']);
+            return $dbh;
+        } catch (\PDOException $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+    public function getPDO(){
+        $this->pdo = $this->connect();
+        return $this->pdo;
+    }
+
 }
