5e1bd233df84fed3018b883f313be645a931832f
Julien-Rtg <julien.rittl@gmail.com>
2022-03-22 09:05:38 +0100

    pagination

--- controllers/AllBlogPostsController.php
+++ controllers/AllBlogPostsController.php
@@ -11,19 +11,38 @@ class AllBlogPostsController extends MainController
 
     private $postModel;
 
+
     public function allBlogPosts()
     {
         $this->postModel = new PostsModel();
-        $posts = $this->postModel->getAllBlogPosts();
-        $this->renderAllBlogPosts($posts ?? null);
+        // $posts = $this->postModel->getAllBlogPosts();
+
+        if (isset($_GET['page']) && !empty($_GET['page'])) { // ON RECUP LA PAGE
+            $currentPage = (int) strip_tags($_GET['page']);
+        } else {
+            $currentPage = 1;
+        }
+
+        $countPosts = $this->postModel->countBlogPosts();
+        $countPosts = intval($countPosts[0]['nb_posts']);
+        // On détermine le nombre d'articles par page
+        $perPage = 5;
+        // On calcule le nombre de pages total
+        $pages = ceil($countPosts / $perPage);
+        $firstPost = ($currentPage * $perPage) - $perPage;
+        $posts = $this->postModel->getLimitBlogPosts($firstPost, $perPage);
+
+        $this->renderAllBlogPosts($posts ?? null, $currentPage ?? null, $pages??null);
     }
 
     // $this->main_view = ./views/main.html/twig and is define in MainController
-    public function renderAllBlogPosts($posts)
+    public function renderAllBlogPosts($posts, $currentPage, $pages)
     {
         echo $this->twig->render($this->main_view, [
             'body' => 'twig/AllBlogPosts.html.twig',
-            'posts' => $posts
+            'posts' => $posts,
+            'currentPage' => $currentPage,
+            'pages' => $pages
         ]);
     }
 
