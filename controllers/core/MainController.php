8fb865d653a74a98e2fc2380f94a4646985b5e66
Julien-Rtg <julien.rittl@gmail.com>
2022-09-13 15:15:32 +0200

    test db, fix twig

--- controllers/core/MainController.php
+++ controllers/core/MainController.php
@@ -1,8 +1,8 @@
 <?php
 namespace Controllers\core;
 
-use Twig_Environment;
-use Twig_Loader_Filesystem;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 use Controllers\core\MailController;
 use Models\AccountsModel;
 
@@ -16,8 +16,8 @@ class MainController{
     protected $account_model;
 
     public function __construct(){
-        $this->twig_loader = new Twig_Loader_Filesystem('./views');
-        $this->twig = new Twig_Environment($this->twig_loader);
+        $this->twig_loader = new FilesystemLoader('./views');
+        $this->twig = new Environment($this->twig_loader);
         $this->main_view = 'main.html.twig';
         $this->mail = new MailController();
         $this->account_model = new AccountsModel();
